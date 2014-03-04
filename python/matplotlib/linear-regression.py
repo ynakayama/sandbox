9b9ec6c3a6c73431bd940f089c29b048c869818e
ynakayama <ya-nakayama@dts.co.jp>
2014-03-04 15:13:01 +0900

    Refactoring, rotate graph

--- python/matplotlib/linear-regression.py
+++ python/matplotlib/linear-regression.py
@@ -3,8 +3,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.array([24, 27, 29, 34, 42, 43, 51])
-X = np.array([236, 330, 375, 392, 460, 525, 578])
+v1 = np.array([24, 27, 29, 34, 42, 43, 51])
+v2 = np.array([236, 330, 375, 392, 460, 525, 578])
 
 def phi(x):
     return [1, x, x**2, x**3]
@@ -12,22 +12,26 @@ def phi(x):
 def f(w, x):
     return np.dot(w, phi(x))
 
-PHI = np.array([phi(x) for x in X])
-w = np.linalg.solve(np.dot(PHI.T, PHI), np.dot(PHI.T, t))
+PHI = np.array([phi(x) for x in v2])
+w = np.linalg.solve(np.dot(PHI.T, PHI), np.dot(PHI.T, v1))
 
-plt.xlim(200, 600)
-plt.ylim(20, 55)
-plt.plot(X, t, 'o', color="blue")
+plt.xlim(20, 55)
+plt.ylim(200, 600)
+plt.xlabel('Age')
+plt.ylabel('Price')
+plt.plot(v1, v2, 'o', color="blue")
 plt.show()
 plt.savefig("image.png")
 
-xlist = np.arange(200, 600, 10)
-ylist = [f(w, x) for x in xlist]
+ylist = np.arange(200, 600, 10)
+xlist = [f(w, x) for x in ylist]
 
 plt.plot(xlist, ylist, color="red")
-plt.xlim(200, 600)
-plt.ylim(20, 55)
-plt.plot(X, t, 'o', color="blue")
+plt.xlim(20, 55)
+plt.ylim(200, 600)
+plt.xlabel('Age')
+plt.ylabel('Price')
+plt.plot(v1, v2, 'o', color="blue")
 plt.show()
 plt.savefig("image2.png")
 
