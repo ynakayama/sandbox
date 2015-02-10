749da60d70f73f8a50bb5ebf1784e1a81055b8cf
Y.Nakayama <ya-nakayama@dts.co.jp>
2015-02-10 19:09:23 +0900

    FA example

--- r/stats/fa.R
+++ r/stats/fa.R
@@ -1,8 +1,14 @@
 df = read.csv("sample_data.csv")
+
 row.names(df) = df[,1]
 df = df[,2:8]
 head(df)
+
 png("image.png", width = 480, height = 480, pointsize = 12, bg = "white", res = NA)
 par(xpd=TRUE)
 biplot(prcomp(df, scale=TRUE))
 dev.off()
+
+result <- factanal(x = df, factors = 2)
+result
+1 - result$uniquenesses
