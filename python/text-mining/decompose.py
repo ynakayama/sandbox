f341ae56f80f38ef6d73e89b154897e5191ea867
Y.Nakayama <ya-nakayama@dts.co.jp>
2021-07-06 15:49:43 +0900

    Decompose nouns

---
+++ python/text-mining/decompose.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import json
+import re
+from collections import OrderedDict
+import MeCab
+
+
+class Analyzer:
+
+    def __init__(self, args):
+        self.path = args[1]
+        self.exclude_list = []
+        self.category_num = 0
+
+    def tagger(self):
+        return MeCab.Tagger('-d /usr/local/lib/mecab/dic/ipadic')
+
+    def preprocessing(self, sentence):
+        return sentence.rstrip()
+
+    def start(self):
+        tagger = self.tagger()
+        with open(self.path) as fd:
+            nouns = []
+            for sentence in map(self.preprocessing, fd):
+                for chunk in tagger.parse(sentence).splitlines()[:-1]:
+                    (surface, feature) = chunk.split('\t')
+                    if feature.startswith('名詞'):
+                        nouns.append(surface)
+        return nouns
+
+
+if __name__ == '__main__':
+    argsmin = 1
+    version = (3, 0)
+    if sys.version_info > (version):
+        if len(sys.argv) > argsmin:
+            analyzer = Analyzer(sys.argv)
+            noun = analyzer.start()
+            print(noun)
+        else:
+            print("This program needs at least %(argsmin)s arguments" %
+                  locals())
+    else:
+        print("This program requires python > %(version)s" % locals())
+
+
